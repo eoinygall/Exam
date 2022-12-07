343f0f11684060c5c50d05be4b25f8e7a5de7491
eoinygall <g00392003@atu.ie>
2022-12-07 11:54:25 +0000

    Fixed pom structure issues and made both class and test class

---
+++ src/test/java/atu/ie/EmployeeTest.java
@@ -0,0 +1,17 @@
+package atu.ie;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
